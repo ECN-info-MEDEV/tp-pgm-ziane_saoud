bd8f194b83463038c45337445f11274d00da2613
zianeabdelhadi <zianeabdelhadi@gmail.com>
2021-12-02 09:07:45 +0100

    Create java project

---
+++ src/main/java/edu/centralenantes/tp/pgm/ziane_saoud/ClassMain.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.centralenantes.tp.pgm.ziane_saoud;
+
+/**
+ *
+ * @author X
+ */
+public class ClassMain {
+    
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
